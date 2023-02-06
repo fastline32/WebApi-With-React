e6561473bbbc0809c105c5f49afccd0f9b0e61ad
fastline32 <speednet@mail.bg>
2023-02-06 19:01:05 +0200

    Creating React Client

---
+++ client-app/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Header,  List} from 'semantic-ui-react';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
+
+function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      setActivities(response.data);
+    })
+  },[])
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
+          {activities.map((activity: any) => (
+            <List.Item key={activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+        </List>
+    </div>
+  );
+}
+
+export default App;
