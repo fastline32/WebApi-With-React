f3acf6c92b03e0ed5df9c51b255acd50458409df
fastline32 <speednet@mail.bg>
2023-03-26 15:25:42 +0300

    Next Changes

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,11 +15,12 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities,selectedActivity,
     selectActivity,cancleSelectActivity,
-    deleteActivity,editMode,openForm,closeForm,createOrEdit}:Props) {
+    deleteActivity,editMode,openForm,closeForm,createOrEdit, submitting}:Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -27,6 +28,7 @@ export default function ActivityDashboard({activities,selectedActivity,
                 activities={activities} 
                 selectActivity={selectActivity}
                 deleteActivity={deleteActivity}
+                submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -37,7 +39,12 @@ export default function ActivityDashboard({activities,selectedActivity,
                     openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm 
+                    closeForm={closeForm}
+                    activity={selectedActivity} 
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}
+                />}
             </Grid.Column>
         </Grid>
     )
