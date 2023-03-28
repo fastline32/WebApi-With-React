97c76060945ae8055a5090eef93ab570cfd5c2b6
fastline32 <speednet@mail.bg>
2023-03-28 20:41:21 +0300

    Styling with Semantic React

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,9 +1,19 @@
-import { Container } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
 
 export default function HomePage() {
     return (
-        <Container style={{marginTop: '7em'}}>
-            <h1>Home Page</h1>
-        </Container>
+       <Segment inverted textAlign='center' vertical className='masthead'>
+            <Container text>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}}/>
+                    Reactivities
+                </Header>
+                <Header as='h2' inverted content='Welcome to Reactivities'/>
+                <Button as={Link} to='/activities' size='huge' inverted>
+                    Take me to the activities
+                </Button>
+            </Container>
+       </Segment>
     )
 }
