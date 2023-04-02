c5b076f9932f52a3c8d635941b012d70bc1b9d01
fastline32 <speednet@mail.bg>
2023-04-02 13:36:02 +0300

    Exception handling finished

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.MiddleWare;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 using Persistence.Data;
@@ -13,6 +14,8 @@ builder.Services.ApplicationServiceExtensions(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
