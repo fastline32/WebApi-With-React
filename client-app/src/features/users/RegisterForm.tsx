b5afc5ac54edbd7f3ade6a1b227b894b3873cb2a
fastline32 <speednet@mail.bg>
2023-04-12 12:25:26 +0300

    Login ang Register in Client side

---
+++ client-app/src/features/users/RegisterForm.tsx
@@ -0,0 +1,42 @@
+import { ErrorMessage, Form, Formik } from "formik";
+import TextInput from "../../app/common/form/TextInpust";
+import { Button, Header } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
+import * as Yup from 'yup';
+import ValidationError from "../errors/ValidationError";
+
+export default observer(function RegisterForm() {
+    const {userStore} = useStore();
+    return (
+        <Formik
+            initialValues={{displayName: '', username: '', email: '', password: '', error: null}}
+            onSubmit={(values, {setErrors}) => userStore.register(values).catch(error => setErrors({error}))}
+            validationSchema={Yup.object({
+                displayName: Yup.string().required(),
+                username: Yup.string().required(),
+                email: Yup.string().required(),
+                password: Yup.string().required(),
+            })}
+        >
+            {({handleSubmit, isSubmitting, errors, isValid , dirty}) => (
+                <Form className='ui form error' onSubmit={handleSubmit} autoComplete='off'>
+                    <Header as='h2' content='Sign Up to Activities' color='teal' textAlign='center'/>
+                    <TextInput placeholder='Display Name' name='displayName' />
+                    <TextInput placeholder='Username' name='username' />
+                    <TextInput placeholder='Email' name='email' />
+                    <TextInput placeholder='Password' name='password' type='password'/>
+                    <ErrorMessage 
+                        name='error' render={() => <ValidationError errors={errors.error}/>}
+                    />
+                    <Button 
+                        disabled={!isValid || !dirty || isSubmitting}
+                        loading={isSubmitting} 
+                        positive content='Register' 
+                        type='submit' fluid 
+                    /> 
+                </Form>
+            )}
+        </Formik>
+    )
+})
